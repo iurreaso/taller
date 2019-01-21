222deab20e8f23ccb95e9275c04dc11e25d474be
ANTONIOMBM <ivanurrea@ext.inditex.com>
2019-01-21 19:00:12 +0100

    inicio

---
+++ demo/src/main/java/com/example/demo/Saludo.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Saludo {
+	
+	private final long id;
+    private final String content;
+
+    public Saludo(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
